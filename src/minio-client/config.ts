2cc6e80e35f81fd653cc5e610defd4a0c07c10d0
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2023-01-15 19:50:07 +0000

    Mino setup

---
+++ src/minio-client/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  MINIO_ENDPOINT: 'localhost',
+  MINIO_PORT: 9000,
+  MINIO_ACCESSKEY: 'w9eFjGej0AWSlXNW',
+  MINIO_SECRETKEY: 'JrsoK0g1HVfKXNrxY4vAytoqGKj4mncQ',
+  MINIO_BUCKET: 'ishok.dev',
+};
